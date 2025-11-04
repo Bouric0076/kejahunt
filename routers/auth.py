b935502c1d58cbc12bb1602ca5b0131a7bc7f9cc
Josphat Munene <munenejosphat667@gmail.com>
2025-11-04 21:34:34 +0300

    Updated testing email creditials and updated main.py

--- routers/auth.py
+++ routers/auth.py
@@ -20,13 +20,13 @@ SMTP_HOST = os.getenv("SMTP_HOST")
 SMTP_PORT = int(os.getenv("SMTP_PORT", 587))
 
 conf = ConnectionConfig(
-    MAIL_USERNAME=SMTP_USER,
-    MAIL_PASSWORD=SMTP_PASSWORD,
-    MAIL_FROM=SMTP_USER,
-    MAIL_PORT=SMTP_PORT,
-    MAIL_SERVER=SMTP_HOST,
-    MAIL_TLS=True,
-    MAIL_SSL=False,
+    MAIL_USERNAME=os.getenv("MAIL_USERNAME"),
+    MAIL_PASSWORD=os.getenv("MAIL_PASSWORD"),
+    MAIL_FROM=os.getenv("MAIL_FROM"),
+    MAIL_PORT=int(os.getenv("MAIL_PORT", "587")),
+    MAIL_SERVER=os.getenv("MAIL_SERVER"),
+    MAIL_SSL_TLS=False,  # Changed from MAIL_SSL
+    MAIL_STARTTLS=True,  # Changed from MAIL_TLS
     USE_CREDENTIALS=True
 )
 
